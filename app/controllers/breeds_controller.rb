a9c34adba51d2abb4829585a16d000510493a0cb
Darren Campbell <darren2802@gmail.com>
2019-12-07 14:39:40 -0700

    Remove files no longer needed

--- app/controllers/breeds_controller.rb
+++
@@ -1,8 +0,0 @@
-class BreedsController < ApplicationController
-  include ActionView::Helpers::TextHelper
-
-  def index
-    breeds = Pet.order(:breed).distinct.pluck(:breed)
-    @breeds_pluralized = breeds.map { |breed| pluralize(breed) }
-  end
-end
