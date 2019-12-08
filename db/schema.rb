c9b249745f8b917ddd0df4ff412e098ce7f6680f
Daniel <danmoran.pro@gmail.com>
2019-12-08 16:36:25 -0700

    update schema for pet_application

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191208221809) do
+ActiveRecord::Schema.define(version: 20191208231226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20191208221809) do
     t.text "motivation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "pet_applications", force: :cascade do |t|
+    t.bigint "pet_id"
+    t.bigint "application_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["application_id"], name: "index_pet_applications_on_application_id"
+    t.index ["pet_id"], name: "index_pet_applications_on_pet_id"
   end
 
   create_table "pets", force: :cascade do |t|
@@ -62,6 +71,8 @@ ActiveRecord::Schema.define(version: 20191208221809) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pet_applications", "applications"
+  add_foreign_key "pet_applications", "pets"
   add_foreign_key "pets", "shelters"
   add_foreign_key "reviews", "shelters"
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -13,5 +13,6 @@ describe Pet, type: :model do
 
   describe "relationships" do
     it {should belong_to :shelter}
+    it {should have_many(:applications).through(:pet_applications)}
   end
 end
