7312e5f17be225dbfed564dbeb8611718dd727c8
Daniel <danmoran.pro@gmail.com>
2019-12-08 17:16:09 -0700

    complete testing of sad path missing information in field. userstory 17

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationsController < ApplicationController
   def new
-    @pet_favorites = Hash.new()
-
-    favorite.contents.each_key do |key|
-      @pet_favorites[key] = Hash.new()
-      @pet_favorites[key]['name'] = Pet.find(key).name
-    end
-    @pet_favorites
+    @pet_favorites = populate_pet_favorites
   end
 
   def create
@@ -19,16 +13,30 @@ class ApplicationsController < ApplicationController
 
     new_app = Application.create(application_params)
 
-
-    @favorite = Favorite.new(session[:favorite])
-    pets_applied_for.each do |pet_id|
-      @favorite.delete_pet(pet_id)
+    if new_app.save
+      flash[:notice] = 'Thank you for applying to adopt'
+      @favorite = Favorite.new(session[:favorite])
+      pets_applied_for.each do |pet_id|
+        new_app.pets << Pet.find(pet_id)
+        @favorite.delete_pet(pet_id)
+      end
+      redirect_to '/favorites'
+    else
+      flash[:notice] = 'Form incomplete please try again'
+      @pet_favorites = populate_pet_favorites
+      render :new
     end
-
-
-    flash[:notice] = 'Thank you for applying to adopt'
-    redirect_to '/favorites'
   end
+
+  def populate_pet_favorites
+    pet_favorites = Hash.new()
+    favorite.contents.each_key do |key|
+      pet_favorites[key] = Hash.new()
+      pet_favorites[key]['name'] = Pet.find(key).name
+    end
+    pet_favorites
+  end
+
 
   private
     def application_params
