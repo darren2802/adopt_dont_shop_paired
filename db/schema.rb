2fbe089b1004ad75e9e6343425d915b3c99c99cc
Darren Campbell <darren2802@gmail.com>
2019-12-10 15:14:25 -0700

    Add application_approved column to pets, remove adoptable column from pets, update seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191210142048) do
+ActiveRecord::Schema.define(version: 20191210213550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,9 +45,9 @@ ActiveRecord::Schema.define(version: 20191210142048) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "shelter_id"
-    t.boolean "adoptable"
     t.string "breed"
     t.text "description"
+    t.boolean "application_approved", default: false
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
