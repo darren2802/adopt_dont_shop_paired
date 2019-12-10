bcb9b312197ead66e39a9f1b9e32cfa8fe385b8f
Darren Campbell <darren2802@gmail.com>
2019-12-10 07:34:09 -0700

    Add approved column to applications table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191208230609) do
+ActiveRecord::Schema.define(version: 20191210142048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20191208230609) do
     t.text "motivation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "approved", default: false
   end
 
   create_table "pet_applications", force: :cascade do |t|
