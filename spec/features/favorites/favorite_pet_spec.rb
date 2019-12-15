862eac469fe63c8cc983091ccc66d7864b883b01
Darren Campbell <darren2802@gmail.com>
2019-12-15 11:09:28 -0700

    Uncomment lines to get test for favorites link in navbar working

--- spec/features/favorites/favorite_pet_spec.rb
+++ spec/features/favorites/favorite_pet_spec.rb
@@ -2,27 +2,27 @@ require 'rails_helper'
 
 RSpec.describe 'favorite Pet', type: :feature do
   before :each do
-    @shelter_1 = Shelter.create( name: 'Dog Haven',
-                                address: '123 Curtis Street',
-                                city: 'Denver',
-                                state: 'Colorado',
-                                zip: 80202)
+    @shelter_1 = Shelter.create(  name: 'Dog Haven',
+                                  address: '123 Curtis Street',
+                                  city: 'Denver',
+                                  state: 'Colorado',
+                                  zip: 80202)
 
-    @pet_1 = Pet.create( name: 'Elvis',
-                        image: 'https://adopt-dont-shop.s3-us-west-1.amazonaws.com/images/border_collie_92.jpg',
-                        age_approx: 7,
-                        sex: 'male',
-                        breed: 'Border Collie',
-                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                        shelter_id: @shelter_1.id)
+    @pet_1 = Pet.create(          name: 'Elvis',
+                                  image: 'https://adopt-dont-shop.s3-us-west-1.amazonaws.com/images/border_collie_92.jpg',
+                                  age_approx: 7,
+                                  sex: 'male',
+                                  breed: 'Border Collie',
+                                  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                                  shelter_id: @shelter_1.id)
 
-      @pet_2 = Pet.create( name: 'Costello',
-                        image: 'https://adopt-dont-shop.s3-us-west-1.amazonaws.com/images/african_hunting_dog_29.jpg',
-                        age_approx: 10,
-                        sex: 'male',
-                        shelter_id: @shelter_1.id,
-                        breed: 'German Shepherd',
-                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+    @pet_2 = Pet.create(          name: 'Costello',
+                                  image: 'https://adopt-dont-shop.s3-us-west-1.amazonaws.com/images/african_hunting_dog_29.jpg',
+                                  age_approx: 10,
+                                  sex: 'male',
+                                  shelter_id: @shelter_1.id,
+                                  breed: 'German Shepherd',
+                                  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
 
   end
   it 'see favorite indicator in navigation bar' do
@@ -42,15 +42,12 @@ RSpec.describe 'favorite Pet', type: :feature do
 
   end
 
-  # it 'favorite indicator in navigation bar directs to favorites index page' do
-  #   visit("/pets/#{@pet_1.id}")
-  #
-  #   click_link 'Favorite This Pet'
-  #   binding.pry
-  #   click_link "Favorited Pets: 3"
-  #   binding.pry
-  #   expect(current_path).to eq("/favorites")
-  # end
+  it 'favorite indicator in navigation bar directs to favorites index page' do
+    visit("/pets/#{@pet_1.id}")
+    click_link 'Favorite This Pet'
+    click_link "Favorited Pets: 1"
+    expect(current_path).to eq("/favorites")
+  end
 end
 
 # As a visitor
